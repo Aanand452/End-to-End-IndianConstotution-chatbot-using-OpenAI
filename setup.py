de8e8a57c150687c73c2f72eb0b219cd4fcec25d
Anand452 <anandkumars016@gmail.com>
2024-08-18 20:11:07 -0400

    adding my few files

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages,setup
+
+
+setup(
+    name = "Indian Constitution AI Project",
+    version="0.0.0",
+    author = "Anandkumar Sivalanka",
+    author_email="anandkumars016@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
